17ca5d6705b5bd1e55b554945fc9e00613559a1f
Camilo Lopez <camilino123@gmail.com>
2019-03-28 15:23:56 -0500

    application.properties fiexd, Properties to create custom querys added

--- src/main/java/com/easyaccess/EasyaccessApplication.java
+++ src/main/java/com/easyaccess/EasyaccessApplication.java
@@ -1,13 +1,23 @@
 package com.easyaccess;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoOperations;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) //This enable connection to mLab socket
 public class EasyaccessApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EasyaccessApplication.class, args);
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        MongoOperations mongoOperation = (MongoOperations) applicationContext.getBean("mongoTemplate");
     }
 
 }
