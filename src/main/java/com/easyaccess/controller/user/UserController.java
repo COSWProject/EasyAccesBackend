e40fc712e81f51ac8b15610b8fe834293b2efcc2
danielagon <daniela.gonzalezramirez@hotmail.com>
2019-04-03 02:44:47 -0500

    User token created and user controller modified

--- src/main/java/com/easyaccess/controller/user/UserController.java
+++ src/main/java/com/easyaccess/controller/user/UserController.java
@@ -5,6 +5,7 @@ import com.easyaccess.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,8 +24,29 @@ public class UserController {
 
     @GetMapping("email/{email}")
     @ResponseBody
-    public User getByEmail(@PathVariable("email") String email) {
-        return userService.getByEmail(email);
+    public User getByEmail(@PathVariable("email") String email) { return userService.getByEmail(email); }
+
+    @GetMapping("userbyemail/{email}")
+    @ResponseBody
+    public String getUserByEmail (@PathVariable("email") String email) {
+        System.out.println("ENTRA");
+        List<String> user = new ArrayList<>();
+        User u = userService.getByEmail(email);
+        user.add(u.getName());
+        user.add(u.getPassword());
+        user.add(u.getId());
+        user.add(u.getEmail());
+        user.add(String.valueOf(u.getMobilePhone()));
+        user.add(u.getAddress());
+        user.add(String.valueOf(u.getHomePhone()));
+        user.add(u.getOccupation());
+        user.add(u.getOrganization());
+        user.add(u.getCity());
+        user.add(u.getState());
+        user.add(u.getCountry());
+        user.add(String.valueOf(u.getPostalCode()));
+        return u.getName();
+
     }
 
     @GetMapping("all")
